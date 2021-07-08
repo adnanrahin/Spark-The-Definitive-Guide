8ad11612885ce51a6d9fd3a3f812f31d608634e7
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-08 14:15:19 -0400

    DataFrames Json File Parsing

---
+++ src/main/scala/apache/spark/apis/structuredapis/LoadingJsonInDataFrames.scala
@@ -0,0 +1,26 @@
+package apache.spark.apis.structuredapis
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object LoadingJsonInDataFrames {
+
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("LoadingJsonInDataFrames")
+      .getOrCreate()
+
+    val df = spark.read.format("json")
+      .load("data/flight-data/json/2015-summary.json")
+
+    df.printSchema()
+
+    df.foreach(row => println(row))
+
+  }
+
+}
