121a95305cc245533ee1077d1caa5496a0010a41
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-12 01:43:16 -0400

    update

---
+++ src/main/scala/apache/spark/apis/readfromcluster/ReadFileFromHdfsCluster.scala
@@ -0,0 +1,30 @@
+package apache.spark.apis.readfromcluster
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.sql.SparkSession
+
+import java.net.URI
+
+object ReadFileFromHdfsCluster {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("ReadFileFromHdfsCluster")
+      .getOrCreate()
+
+    val fs = FileSystem.get(new URI("hdfs://localhost:9000/"), new Configuration())
+
+    val df = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("hdfs://localhost:9000/data/retail-data/by-day/2010-12-01.csv")
+
+    println(df.show(20))
+
+  }
+
+}
