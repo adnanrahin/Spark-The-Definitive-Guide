e64ea9916f55cd82226f229c538dc68bddd9e033
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-18 04:35:44 -0400

    update

--- src/main/scala/apache/spark/apis/rdd/CreatingRDD.scala
+++ src/main/scala/apache/spark/apis/rdd/CreatingRDD.scala
@@ -1,10 +1,13 @@
 package apache.spark.apis.rdd
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object CreatingRDD {
 
   def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
 
     val spark = SparkSession
       .builder()
@@ -14,7 +17,16 @@ object CreatingRDD {
 
     val rdd = spark.range(500).rdd
 
-    println(rdd)
+    rdd.foreach(row => println(row))
+
+    /*parallelize method on a SparkContext (within a SparkSession). */
+
+    val myCollection = "Spark The Definitive Guide : Big Data Processing Made Simple"
+      .split(" ")
+
+    val words = spark.sparkContext.parallelize(myCollection ,2)
+
+    words.foreach(word => println(word))
 
   }
 
