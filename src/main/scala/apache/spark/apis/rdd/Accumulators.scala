7211b2dbebb1c931e3e93bc19404175c8878024c
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-23 19:28:48 -0400

    #1 fixed

--- src/main/scala/apache/spark/apis/rdd/Accumulators.scala
+++ src/main/scala/apache/spark/apis/rdd/Accumulators.scala
@@ -1,8 +1,10 @@
 package apache.spark.apis.rdd
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Encoders, SparkSession}
 
 object Accumulators {
+
+  case class Flight(DEST_COUNTRY_NAME: String, ORIGIN_COUNTRY_NAME: String, count: BigInt)
 
   def main(args: Array[String]): Unit = {
 
@@ -12,7 +14,18 @@ object Accumulators {
       .appName("Accumulators")
       .getOrCreate()
 
+    /*val df = spark.read.format("parquet")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("hdfs://localhost:9000/data/flight-data/parquet/2010-summary.parquet")
 
+    println(df.show(40))*/
+
+    val flightsDf = spark.read
+      .parquet("hdfs://localhost:9000/data/flight-data/parquet/2010-summary.parquet")
+      val flights = flightsDf.as(Encoders.bean(Flight.getClass))
+
+    println(flights.show(20))
 
   }
 
